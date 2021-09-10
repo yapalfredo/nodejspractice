7cb5f934a95db7da6e86b3dbbdf2f376349d716e
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-10 15:46:47 -0400

    insert to mongodb working

--- todo-app/server.js
+++ todo-app/server.js
@@ -20,57 +20,53 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
 app.use(express.urlencoded({extended: false}))
 
 app.get("/", function(req, res){
-    res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Simple To-Do App</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container">
-        <h1 class="display-4 text-center py-1">To-Do App</h1>
-        
-        <div class="jumbotron p-3 shadow-sm">
-          <form action="/add-item" method="POST">
-            <div class="d-flex align-items-center">
-              <input name="todoItem" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-              <button class="btn btn-primary">Add New Item</button>
+    //mongodb way of saying Read or Load -> find()
+    db.collection('todolists').find().toArray(function(err, items){
+        res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Simple To-Do App</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        </head>
+        <body>
+          <div class="container">
+            <h1 class="display-4 text-center py-1">To-Do App</h1>
+            
+            <div class="jumbotron p-3 shadow-sm">
+              <form action="/add-item" method="POST">
+                <div class="d-flex align-items-center">
+                  <input name="todoItem" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                  <button class="btn btn-primary">Add New Item</button>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-        
-        <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-        </ul>
-        
-      </div>
-      
-    </body>
-    </html>
-    `)
+            
+            <ul class="list-group pb-5">
+                ${items.map(function(i){
+                    //this will dynamically load each item from the mongodb database
+                    //the 'i' holds the value from the array
+                    //the .join() will separate each item by empty space
+                    return `
+                    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                        <span class="item-text">${i.text}</span>
+                        <div>
+                            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                            <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                        </div>
+                    </li>
+                    `
+                }).join('')}
+            </ul>
+            
+          </div>
+          
+        </body>
+        </html>
+        `)  
+    })
 })
 
 //this listener will response to the user
@@ -80,7 +76,10 @@ app.post("/add-item", function(req, res){
 
     //INSERT
     db.collection("todolists").insertOne({text: req.body.todoItem}, function(){
-        res.send("this listener is working")
+        //res.send("this listener is working")
+
+        //this redirects back to home everytime an item is added
+        res.redirect('/')
     })
 
 })
